297c276637b21735b9e8deaed3c2995d3dca0145
Natalie11K <46880424+Natalie11K@users.noreply.github.com>
2019-01-28 12:52:14 +0800

    Add files via upload

---
+++ p5jsTutorial1.2-master/sketch.js
@@ -0,0 +1,42 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+	background(255,0,0)
+
+	// change the paint brush to a specific color according to RGB values
+	// this color applies only to the inside color of a shape
+	fill(157,231,255);
+
+	// change the paint brush to a specific color according to RGB values
+	// this color applies only to the outside color of a shape
+  // work with Jessica
+	stroke(87,221,236);
+
+	// modifies the thickness of the border of an object
+	strokeWeight(10);
+
+
+	rect(10,10,100,50);
+	rect(10,400,100,50);
+
+
+	fill(50,255,20);
+	stroke(255,255,255);
+	strokeWeight(20);
+
+	rect(580,10,100,50);
+	rect(350,180,100,50);
+
+fill(0,0,0)
+stroke(254,254,254)
+strokeWeight(25)
+  rect(600,400,100,50)
+  rect(490,400,100,50)
+
+
+}
