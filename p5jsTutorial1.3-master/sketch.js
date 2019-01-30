f3bde2057a342a5b728e3f6448f2c12b69dfa79e
Natalie11K <46880424+Natalie11K@users.noreply.github.com>
2019-01-30 11:33:26 +0800

    Add files via upload

---
+++ p5jsTutorial1.3-master/sketch.js
@@ -0,0 +1,30 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+	background(255,0,0)
+
+	fill(0,0,255);
+	stroke(255,255,255);
+	strokeWeight(2);
+
+	text("Hello World",80,30);
+
+	rect(150,10,100,50);
+
+	fill(0,0,255);
+
+	text("Donald Trump",30,130);
+rect(10,100,100,50);
+
+fill(204,153,255);
+stroke(0,0,0);
+strokeWeight(3);
+
+text("Natalie",100,359);
+
+}
