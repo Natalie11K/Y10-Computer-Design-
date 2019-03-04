37e7d4600e0c80ac08bbc756d4d4723addf75630
Natalie11K <46880424+Natalie11K@users.noreply.github.com>
2019-03-04 13:10:32 +0800

    Add files via upload

--- Button Class/buttonList/sketch.js
+++ Button Class/buttonList/sketch.js
@@ -1,18 +1,25 @@
 var b1;
 var b2;
+var b3;
 
 function setup()
 {
 	createCanvas(500,500);
 	b1 = new Button(10,10,100,25);
-	b1.setText("index");
-	b1.setTextOver("GO");
+	b1.setText("Menu");
+	b1.setTextOver("Instructions");
 	b1.setButtonOverFill(255,255,50);
 
-	b2 = new Button(10,100,100,25);
-	b2.setText("Hong Kong");
-	b2.setTextOver("Phone");
+	b2 = new Button(10,70,100,25);
+	b2.setText("Menu");
+	b2.setTextOver("Easy");
 	b2.setButtonOverFill(255,255,50);
+
+	b3 = new Button(10,100,100,25);
+	b3.setText("Menu");
+	b3.setTextOver("Hard");
+	b3.setButtonOverFill(255,255,50);
+
 }
 
 function draw()
@@ -21,6 +28,7 @@ function draw()
 
 	b1.showButton();
 	b2.showButton();
+	b3.showButton();
 
 	if (b1.getButtonState() == 1)
 	{
--- Button Class/sketch.js
+++ Button Class/sketch.js
@@ -9,11 +9,26 @@ function setup()
 	//initializing the button 1 with
 	//x,y,length, height values
 	b1 = new Button(10,10,100,25);
-	b1.setText("Natalie");
-	b1.setTextOver("VSA");
+	b1.setText("Main Menu");
+	b1.setTextOver("Instructions on how to play game");
+	b1.setButtonOverFill(249,56,249);
+
+}
+var b1;
+
+{
+	b1 = new Button(10,10,100,25);
+	b1.setText("Main Menu");
+	b1.setTextOver("Easy");
 	b1.setButtonOverFill(249,56,249);
 }
 
+{
+	b1 = new Button(10,10,100,25);
+	b1.setText("Main Menu");
+	b1.setTextOver("Hard");
+	b1.setButtonOverFill(249,56,249);
+}
 function draw()
 {
 	background(255,255,255);
